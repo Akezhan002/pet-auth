92617510c1e2b106daad81067237912cd108caf2
Akezhan <timurovakt@mail.ru>
2024-07-24 18:38:43 +0500

    иницилизировать логгер

--- cmd/sso/main.go
+++ cmd/sso/main.go
@@ -1,11 +1,54 @@
-package sso
+package main
+
+import (
+	"example.com/pet_auth/internal/config"
+	"example.com/pet_auth/internal/lib/logger/handlers/slogpretty"
+	"log/slog"
+	"os"
+)
+
+const (
+	envLocal = "local"
+	envDev   = "dev"
+	envProd  = "prod"
+)
 
 func main() {
-	// TODO: иницилизировать обьект конфига
+	cfg := config.MustLoad()
 
-	// TODO: иницилизировать логгер
+	log := setupLogger(cfg.Env)
 
+	log.Info("starting application", slog.Any("config", cfg))
 	// TODO: иницилизировать APP
 
 	// TODO: запустить grpc сервер
+}
+
+func setupLogger(env string) *slog.Logger {
+	var log *slog.Logger
+
+	switch env {
+	case envLocal:
+		log = setupPrettySlog()
+	case envDev:
+		log = slog.New(
+			slog.NewJSONHandler(os.Stdout, &slog.HandlerOptions{Level: slog.LevelDebug}),
+		)
+	case envProd:
+		log = slog.New(
+			slog.NewJSONHandler(os.Stdout, &slog.HandlerOptions{Level: slog.LevelInfo}),
+		)
+	}
+	return log
+}
+
+func setupPrettySlog() *slog.Logger {
+	opts := slogpretty.PrettyHandlerOptions{
+		SlogOpts: &slog.HandlerOptions{
+			Level: slog.LevelDebug,
+		},
+	}
+	handler := opts.NewPrettyHandler(os.Stdout)
+
+	return slog.New(handler)
 }
